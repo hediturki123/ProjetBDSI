6569b504cb8ba6e49572c47c82575be4a7c22ec7
hediturki123 <hedi.turki.sanekli@gmail.com>
2021-03-24 17:37:06 +0100

    petite rectif

--- photonum/objects/PhotoAlbum.java
+++ photonum/objects/PhotoAlbum.java
@@ -1,13 +1,8 @@
 package photonum.objects;
 
-import java.sql.Connection;
 
-public class PhotoAlbum extends Photo{
-	private String texteDescriptif;
-
-	public PhotoAlbum(Connection conn) {
-		super(conn);
-	}
+public class PhotoAlbum extends Photo {
+	private String texteDescrptif;
 
 	public PhotoAlbum(int idPhoto, String chemin, String textDescrptif) {
 		super(idPhoto, chemin);
