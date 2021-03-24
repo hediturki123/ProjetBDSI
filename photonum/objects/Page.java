ae8c849a1b9f8cfa9bf567f8eefe46d299bb86c1
l3miage-marinhon <nicomr70@gmail.com>
2021-03-24 19:31:00 +0100

    destroy import

--- photonum/objects/Page.java
+++ photonum/objects/Page.java
@@ -1,10 +1,8 @@
 package photonum.objects;
 import photonum.PhotoNum;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class Page{
 
