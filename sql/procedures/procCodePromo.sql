e9bb85f25c61002b4f3edcbc17a271b1cc4901bf
Alexis YVON <alexis.yvon@gmail.com>
2021-03-26 22:02:17 +0100

    Fix procédure code promo

--- sql/procedures/procCodePromo.sql
+++ sql/procedures/procCodePromo.sql
@@ -4,6 +4,6 @@ CREATE OR REPLACE PROCEDURE code_promo_proc (
 )
 IS
 BEGIN
-    INSERT INTO LesCodesPromo VALUES (in_code, in_mail_client);
+    INSERT INTO LesCodesPromo VALUES (in_code, in_mail_client, 0);
 END;
 /
