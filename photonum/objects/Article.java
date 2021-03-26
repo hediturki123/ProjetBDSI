4d0e7348bf73ef9dd516725a6364437f8fe92bc4
Alexis YVON <alexis.yvon@gmail.com>
2021-03-26 15:56:49 +0100

    Génération facture (2)

--- photonum/objects/Article.java
+++ photonum/objects/Article.java
@@ -1,14 +1,27 @@
 package photonum.objects;
 
+import photonum.PhotoNum;
+import photonum.dao.CommandeDAO;
+import photonum.dao.ImpressionDAO;
+
 public class Article {
 
     private int idCommande;
     private int idImpression;
     private int quantite;
 
+    private final static CommandeDAO C_DAO = new CommandeDAO(PhotoNum.conn);
+    private final static ImpressionDAO I_DAO = new ImpressionDAO(PhotoNum.conn);
+
     public Article(int idCommande, int idImpression, int quantite) {
         setIdCommande(idCommande);
         setIdImpression(idImpression);
+        setQuantite(quantite);
+    }
+
+    public Article(Commande c, Impression i, int quantite) {
+        setIdCommande(c.getIdCommande());
+        setIdImpression(i.getIdImpression());
         setQuantite(quantite);
     }
 
@@ -31,18 +44,35 @@ public class Article {
         this.idImpression = idImpression;
     }
 
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
 
     public int getQuantite() {
         return quantite;
     }
 
+    public Impression getImpression() {
+        return I_DAO.read(idImpression);
+    }
 
-    public void setQuantite(int quantite) {
-        this.quantite = quantite;
+    public Commande getCommande() {
+        return C_DAO.read(idCommande);
     }
 
     @Override
     public String toString() {
         return "Article [idCommande=" + idCommande + ", idImpression=" + idImpression + ", quantite=" + quantite + "]";
+    }
+
+    public String factureString() {
+        Produit p = getImpression().getProduit();
+        String fs = String.join(" | ",
+            p.getReference(),
+            p.getPrix()+"€",
+            quantite+"",
+            p.getPrix()*quantite+"€"
+        );
+        return fs;
     }
 }
